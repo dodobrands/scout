5936997adb083d369877f5c29f1b2ad5b1323800
Aleksey Berezka <a.berezka@dodobrands.io>
2026-02-07 10:35:42 +0500

    Remove unused headCommit(in:) function

--- Sources/Common/Git.swift
+++ Sources/Common/Git.swift
@@ -14,18 +14,6 @@ public enum Git {
         return result.trimmingCharacters(in: .whitespacesAndNewlines)
     }
 
-    /// Returns the current HEAD commit hash in the specified repository.
-    /// - Parameter repoPath: Path to the repository
-    /// - Returns: The HEAD commit hash
-    package static func headCommit(in repoPath: URL) async throws -> String {
-        let result = try await Shell.execute(
-            "git",
-            arguments: ["rev-parse", "HEAD"],
-            workingDirectory: FilePath(repoPath.path(percentEncoded: false))
-        )
-        return result.trimmingCharacters(in: .whitespacesAndNewlines)
-    }
-
     /// Returns the commit timestamp in ISO 8601 format.
     /// - Parameters:
     ///   - hash: Commit hash
